91a6fdd1bfc7c4816780ec65271af559fe4a2f9c
sid <sadhashiva08@gmail.com>
2019-03-20 22:44:53 -0400

    changes added

---
+++ AsyncPrograming/AsyncPrograming/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncPrograming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSync_Click(object sender, RoutedEventArgs e)
+        {
+            UtilMethods util = new UtilMethods();
+            var websitecontents = util.GetWebsiteContentLengthSync();
+
+            listBox.ItemsSource = websitecontents;
+        }
+
+        private void btnasynchronous_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnparallelasynchronous_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btncancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AsyncPrograming/AsyncPrograming/UtilMethods.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncPrograming
+{
+    public class UtilMethods
+    {
+
+        public static List<string> GetWebsites()
+        {
+            List<string> websites = new List<string>();
+
+            websites.Add("https://www.facebook.com/");
+            websites.Add("https://www.google.com/");
+            websites.Add("https://www.twitter.com/");
+            websites.Add("https://www.microsoft.com/");
+            websites.Add("https://www.yahoo.com/");
+            websites.Add("https://www.gmail.com/");
+            websites.Add("https://www.amazon.com/");
+            websites.Add("https://www.citrix.com/");
+
+            return websites;
+        }
+
+        public List<string> GetWebsiteContentLengthSync()
+        {
+            var websites = GetWebsites();
+
+            var websitecontents = DownloadDataSync(websites);
+
+            return websitecontents;
+
+        }
+
+        public List<string> DownloadDataSync(List<string> websites)
+        {
+            List<string> lstwebsiteContent = new List<string>();
+            WebClient client = new WebClient();
+
+            foreach (var site in websites)
+            {
+                var content = client.DownloadString(site);
+                lstwebsiteContent.Add($"The String Count for website {site} is {content.Length}");
+            }
+
+            return lstwebsiteContent;
+
+        }
+
+        public Task<List<string>> DownloadDataAsync(List<string> websites)
+        {
+            List<string> lstwebsiteContent = new List<string>();
+            WebClient client = new WebClient();
+
+            var tasks = new List<Task<string>>();
+
+            foreach (var site in websites)
+            {
+                tasks.Add(client.DownloadStringAsync(site);
+                var content = client.DownloadString(site);
+                lstwebsiteContent.Add($"The String Count for website {site} is {content.Length}");
+            }
+
+            return lstwebsiteContent;
+
+        }
+    }
+}
